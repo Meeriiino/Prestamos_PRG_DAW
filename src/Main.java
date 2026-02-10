081d5a0d8940976349aa4627e7da34a8c3581ce0
FURRULA <mario.sanber.1@educa.jcyl.es>
2026-02-10 11:30:57 +0100

    prueba martes

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
+import java.time.LocalDate;
+
 public class Main {
+  public static void main(String[] args) {
+
+    Usuario u1 = new Usuario("Sergio","sergio332@gmail.com","SOC12345", LocalDate.of(2026,02,8));
+
+
+  }
 }
--- src/Usuario.java
+++ src/Usuario.java
@@ -1,4 +1,5 @@
 import java.time.LocalDate;
+import java.util.InputMismatchException;
 
 public class Usuario {
 
@@ -10,28 +11,52 @@ public class Usuario {
   private LocalDate fechaFinSancion;
 
 
-  try{
-      public Usuario(String nombre, String email, String numeroSocio, LocalDate fechaRegistro) {
-        this.nombre = nombre;
-        this.email = email;
-        this.numeroSocio = numeroSocio;
-        this.fechaRegistro = fechaRegistro;
-  }
-  }
 
-  public void sancionar(){
 
+
+  public Usuario(String nombre, String email, String numeroSocio, LocalDate fechaRegistro) {
+    this.nombre = nombre;
+    this.email = email;
+    this.numeroSocio = numeroSocio;
+    this.fechaRegistro = fechaRegistro;
   }
 
-  public void levantarSancion(){
 
+  public void sancionar(int diasSancion, LocalDate fechaInicio) {
+    this.sancionado = true;
+    this.fechaFinSancion = fechaInicio.plusDays(diasSancion);
   }
 
-  public boolean estaSancionado(boolean sancionado){
 
-
+  public void levantarSancion() {
+    this.sancionado = false;
+    this.fechaFinSancion = null;
   }
 
+  public boolean estaSancionado() {
+    return this.sancionado;
+  }
+
+
+  @Override
+  public String toString() {
+    return "Usuario{" +
+      "nombre='" + nombre + '\'' +
+      ", email='" + email + '\'' +
+      ", numeroSocio='" + numeroSocio + '\'' +
+      ", fechaRegistro=" + fechaRegistro +
+      ", sancionado=" + sancionado +
+      ", fechaFinSancion=" + (fechaFinSancion != null ? fechaFinSancion : "N/A") +
+      '}';
+  }
+
+  public String getNumeroSocio() {
+    return numeroSocio;
+  }
+
+  public LocalDate getFechaFinSancion() {
+    return fechaFinSancion;
+  }
 
 
 
