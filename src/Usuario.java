2e34df799cc031d77b0f1ab66aca356df0680600
FURRULA <mario.sanber.1@educa.jcyl.es>
2026-02-10 11:01:53 +0100

    Primer commit

---
+++ src/Usuario.java
@@ -0,0 +1,36 @@
+import java.time.LocalDate;
+
+public class Usuario {
+
+  private String nombre;
+  private String email;
+  private String numeroSocio;
+  private LocalDate fechaRegistro;
+  private boolean sancionado;
+  private LocalDate fechaFinSancion;
+
+  public Usuario(String nombre, String email, String numeroSocio, LocalDate fechaRegistro) {
+    this.nombre = nombre;
+    this.email = email;
+    this.numeroSocio = numeroSocio;
+    this.fechaRegistro = fechaRegistro;
+  }
+
+  public void sancionar(){
+
+  }
+
+  public void levantarSancion(){
+
+  }
+
+  public boolean estaSancionado(boolean sancionado){
+
+
+  }
+
+
+
+
+
+}
